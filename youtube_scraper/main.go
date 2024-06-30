d7c9dff7544ae72a430b5dae60b4f6443aedfd30
nircoren <114494002+nircoren@users.noreply.github.com>
2024-06-30 23:49:28 +0300

    added cors

--- youtube_scraper/main.go
+++ youtube_scraper/main.go
@@ -19,6 +19,7 @@ func main() {
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Allow requests from React/Next.js frontend
+		w.Header().Set("Access-Control-Allow-Origin", "http://65.109.160.94")  // Allow requests from React/Next.js frontend
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		if r.Method == http.MethodOptions {
